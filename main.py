39c811216b5b8eb2af201bff4d93c34badda0551
Behnam Zeinali <Zeinali.behnam@gmail.com>
2024-04-28 23:55:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ Created on Thu Apr 25 03:10:55 2024
 """
 
 import subprocess
-script_path_0 = '.python/server_model.py'
-script_path_1 = '.python/get_logits.py'
-script_path_2 = '.python/distillation_nas.py'
-script_path_3 = '.python/decision_unit.py'
+script_path_0 = 'server_model.py'
+script_path_1 = 'get_logits.py'
+script_path_2 = 'distillation_nas.py'
+script_path_3 = 'decision_unit.py'
 subprocess.run(['python', script_path_0])
 subprocess.run(['python', script_path_1])
 subprocess.run(['python', script_path_2])
